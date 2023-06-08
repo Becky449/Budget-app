06e9122dff82808aefe22bb273491059e68913a9
Becky449 <rabnab12@gmail.com>
2023-06-08 13:16:35 +0300

    added validation and relationships to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true
     
+    has_many :categories
+    has_many :products
 end
