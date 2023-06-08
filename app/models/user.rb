429405379604c96c9e40c9559388f4a2ee25b224
Becky449 <rabnab12@gmail.com>
2023-06-08 16:43:15 +0300

    Generated controller categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :email, presence: true
-    
-    has_many :categories
-    has_many :products
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+
+  has_many :categories
+  has_many :products
 end
