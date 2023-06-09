68103ce32c5cc013f5739603dfe6a6290ed44db4
Becky449 <rabnab12@gmail.com>
2023-06-09 14:01:37 +0300

    Added category shows and actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-  def index; end
+     load_and_authorize_resource
+  def index; 
+    @categories = current_user.categories
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_url, notice: 'Category was successfully created!.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
